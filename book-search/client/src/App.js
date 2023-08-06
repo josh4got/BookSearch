11e1be986aa3c0e2e4fce5d7cf8ddeb627c72695
josh4got <125082025+josh4got@users.noreply.github.com>
2023-08-06 10:58:38 -0600

    App.js:
    -added apollo provider in app function to allow all request to work with apollo
    -added middleware for jwt authorization to work

--- book-search/client/src/App.js
+++ book-search/client/src/App.js
@@ -1,11 +1,43 @@
+// import dependencies
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// import components
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// establish link to GraphQL server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// middleware function to retrieve token from localStorage and set the request headers before making the GraphQL API request
+const authLink = setContext((_, { headers }) => {
+  // get the token from localStorage
+  const token = localStorage.getItem('id_token');
+  // return headers to context
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// instantiate Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// define App function
 function App() {
   return (
+    // wrap the entire app in ApolloProvider component to make every request work with the ApolloClient
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -25,6 +57,7 @@ function App() {
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
